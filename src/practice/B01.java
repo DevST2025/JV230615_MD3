c79377718e320bb9ec34cb78075b409aa1055ce4
DevST <devst2025@gmail.com>
2023-10-04 06:03:55 +0700

    Submit ss10

--- src/practice/B01.java
+++ src/practice/B01.java
@@ -1,12 +1,24 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class B01 {
     public static void main(String[] args) {
-        List<Integer> arrInt = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        List<Integer> list = new ArrayList<>();
+        int maxLength = 10; // Maximum number of elements in the list
+
+        int length = (int) (Math.random() * maxLength) + 1; // Random length of the list
+
+        for (int i = 0; i < length; i++) {
+            int randomNumber = (int) (Math.random() * 100); // Generate random number between 0 and 99
+            list.add(randomNumber);
         }
+
+        System.out.println("Random List: " + list);
+
+        int maxElement = Collections.max(list);
+        System.out.println("Max Element: " + maxElement);
     }
 }
---
+++ src/practice/B03.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class B03 {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(2);
+        numbers.add(8);
+        numbers.add(1);
+        numbers.add(3);
+
+        System.out.println("Original List: " + numbers);
+
+        Collections.sort(numbers, Collections.reverseOrder());
+
+        System.out.println("Sorted List (Descending): " + numbers);
+    }
+}
