8132e1c3027cdd04708c80aff6bc2581053f5a3d
DevST <devst2025@gmail.com>
2023-10-11 06:44:43 +0700

    submit ss15

--- src/practice/B05.java
+++ src/practice/B05.java
@@ -1,12 +1,11 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+
 public class B05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        try {
         Random random = new Random();
         List<Integer> listNumber = new ArrayList<>();
         System.out.print("Bạn có bao nhiêu số trong mảng: ");
@@ -15,10 +14,38 @@ public class B05 {
             listNumber.add(random.nextInt(100)+1);
         }
         List<Integer> newList = listNumber.stream().sorted().toList();
-        System.out.println(newList);
+        System.out.print("Số mà bạn tìm là: ");
+        int number = Integer.parseInt(sc.nextLine());
+        int result = binarySearch(listNumber, number);
+
+        if (result != -1) {
+            System.out.println("Phần tử " + number + " được tìm thấy tại vị trí " + result);
+        } else {
+            System.out.println("Phần tử " + number + " không được tìm thấy trong mảng.");
+        }
+    } catch (InputMismatchException e) {
+        System.out.println("Lỗi: Nhập không phải là số nguyên.");
+    } finally {
+        sc.close(); // Bước 5: Đóng Scanner
+    }
+    }
+    public static int binarySearch(List<Integer> list, int number) {
+        int left = 0;
+        int right = list.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid) == number) {
+                return mid;
+            } else if (list.get(mid) < number) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+        }
 
 
     }
-
 
 }
--- src/practice/B15.java
+++ src/practice/B15.java
@@ -1,4 +1,38 @@
 package practice;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class B15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double side1, side2, side3;
+
+        try {
+            System.out.print("Nhập cạnh thứ nhất: ");
+            side1 = scanner.nextDouble();
+            System.out.print("Nhập cạnh thứ hai: ");
+            side2 = scanner.nextDouble();
+            System.out.print("Nhập cạnh thứ ba: ");
+            side3 = scanner.nextDouble();
+
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0 || (side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)) {
+                throw new IllegalTriangleException("Các cạnh không thể tạo thành một tam giác.");
+            }
+
+            System.out.println("Ba cạnh đã nhập hợp lệ và tạo thành một tam giác.");
+        } catch (InputMismatchException e) {
+            System.out.println("Lỗi: Bạn phải nhập một số thực.");
+        } catch (IllegalTriangleException e) {
+            System.out.println("Lỗi: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
+}
+class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
 }
